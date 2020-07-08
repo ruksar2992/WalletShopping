466c49b54987136ba48df18ab4b8b25b0abd06d5
ruksar2992 <67260081+ruksar2992@users.noreply.github.com>
2020-07-08 11:02:17 +0530

    basecommit

---
+++ src/main/java/com/example/walletshopping/WalletshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/walletshopping/WalletshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
